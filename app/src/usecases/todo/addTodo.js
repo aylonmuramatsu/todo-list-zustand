cb5a3ba35397132299507a8515e90bedfb7e4e70
Henrique Marciano <rikmarciano@outlook.com.br>
2022-07-20 11:15:10 +0000

    Update addTodo.js

--- app/src/usecases/todo/addTodo.js
+++ app/src/usecases/todo/addTodo.js
@@ -1,6 +1,14 @@
-/* eslint-disable */ 
+import produce from 'immer';
+
 export const addTodo = (set, message) => {
   set((state) => ({
-    todo: [...state.todo, { id: state.todo.length + 1, message, done: false}]
+      //   todo: [...state.todo, { id: state.todo.length + 1, message, done: false}]
+      todo: produce(state.todo, draft => {
+        draft.push({
+          id: draft.length + 1,
+          message,
+          done: false
+        });
+      }),
   }));
 }
