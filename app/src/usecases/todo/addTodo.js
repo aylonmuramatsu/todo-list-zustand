dc279f1cdf95d024293876bd40d8a727fa155123
Aylon Muramatsu <aylon.muramatsu@gmail.com>
2022-07-20 11:32:20 -0300

    Update addTodo.js

--- app/src/usecases/todo/addTodo.js
+++ app/src/usecases/todo/addTodo.js
@@ -2,7 +2,6 @@ import produce from 'immer';
 
 export const addTodo = (set, message) => {
   set((state) => ({
-      //   todo: [...state.todo, { id: state.todo.length + 1, message, done: false}]
       todo: produce(state.todo, draft => {
         if(!message) {
           return draft;
